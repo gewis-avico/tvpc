a1c50f7f1d28ab9637a67f4d1fb102fe1191f7b9
AViCo <avico@gewis.nl>
2016-09-13 16:58:02 +0200

    sutvpc

--- pages/randomposter.php
+++
@@ -1,7 +0,0 @@
-<?php
-$nposters = count($settings['randomposters']);
-$poster = mt_rand(0,$nposters-1);
-if ($nposters>0)
-$image = $settings['randomposters'][$poster];
-?>
-<img class="poster" src="<?=$image;?>">
